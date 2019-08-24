e6add808643184669197d12cbcec9ba73c466636
Birhant <46487373+Birhant@users.noreply.github.com>
2019-08-24 00:17:11 -0700

    the first
    description

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CSETMM</title>
+</head>
+<body>
+
+<!-- Search form -->
+<form class="form-inline md-form form-sm active-cyan-2 mt-2" action="" method="">
+  <input class="form-control form-control-sm mr-3 w-75" type="text" placeholder="Search courses"
+    aria-label="Search">
+  <i class="fas fa-search" aria-hidden="true"></i>
+</form>
+
+</body>
+</html>
+
+
+
+
